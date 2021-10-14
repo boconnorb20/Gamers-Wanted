c25ced5eae046d727fa0b7130a51c420cfbe4713
Saiban Hussein <shhussein@m.marywood.edu>
2021-10-14 08:35:33 -0500

    make some changs in seed file and genre model

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/Gamers-Wanted',
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Gamers-Wanted',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
