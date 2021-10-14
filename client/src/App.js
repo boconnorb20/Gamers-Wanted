d92fd6dd601ba6b553ad26ee6c3ecdd3e4074bfa
Brian OConnor <boconnor20@gmail.com>
2021-10-13 21:06:45 -0500

    fixing app.js

--- client/src/App.js
+++ client/src/App.js
@@ -9,11 +9,9 @@ import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
-import Profile from './pages/Profile';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
-import Header from './components/Header';
-import Footer from './components/Footer';
+
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -52,14 +50,7 @@ function App() {
             <Route exact path="/signup">
               <Signup />
             </Route>
-            <Route exact path="/me">
-              <Profile />
-            </Route>
-            <Route exact path="/profiles/:profileId">
-              <Profile />
-            </Route>
           </div>
-          <Footer />
         </div>
       </Router>
     </ApolloProvider>
